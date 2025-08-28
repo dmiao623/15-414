df996b77531fbcd57adddaa16f3753f87efa9abd
dustinm <dustinmmiao@gmail.com>
2025-08-28 17:15:43 -0400

    updated nix flake to inherit zsh environment

--- flake.nix
+++ flake.nix
@@ -1,14 +1,25 @@
 {
   description = "15-414 assignments development shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  outputs = { self, nixpkgs }: {
-    devShells.default = let
-      pkgs = import nixpkgs { system = "aarch64-darwin"; };
-    in pkgs.mkShell {
-      buildInputs = with pkgs [ 
-        alt-ergo 
+  outputs = { self, nixpkgs }: 
+  let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        alt-ergo
         why3
       ];
+      shellHook = ''
+        [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ] && . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+        exec zsh -l
+
+        why3 config detect
+      '';
     };
   };
 }

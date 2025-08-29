1dcbc095cd1efcad631a57cefc0073decdb262cd
dustinm <dustinmmiao@gmail.com>
2025-08-28 22:34:46 -0400

    changed version of alt-ergo and added cvc4 and gnumake packages

--- flake.nix
+++ flake.nix
@@ -7,18 +7,29 @@
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
-    };
+      overlays = [
+        (final: prev: {
+          alt-ergo = prev.alt-ergo.overrideAttrs (_: {
+            version = "2.6.0";
+            src = prev.fetchurl {
+              url = "https://github.com/OCamlPro/alt-ergo/releases/download/v2.6.0/alt-ergo-2.6.0.tbz";
+              hash = "sha256-EmkxGvJSeKRmiSuoeMyIi6WfF39T3QPxKixiOwP8834=";
+            };
+          });
+        })
+      ];    };
   in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         alt-ergo
+        cvc4
+        gnumake
         why3
       ];
       shellHook = ''
         [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ] && . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-        exec zsh -l
-
         why3 config detect
+        exec zsh -l
       '';
     };
   };
